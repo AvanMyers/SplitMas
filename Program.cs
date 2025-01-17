5ad434ba271805f9ab81c25725b72c83fcdbd7ff
AvanMyers <smail1341@gmail.com>
2025-01-17 10:48:39 +0300

    create

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Дана строка с текстом, используя метод строки String.Split()" +
+                " получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+            string[] subs = text.Split(' ');
+
+            foreach (string wrod in subs)
+                Console.WriteLine(wrod);
+
+            Console.ReadKey();
+        }
+    }
+}
