97c892d5715fb3cc94a5a7881adcd11c00bed777
AvanMyers <smail1341@gmail.com>
2025-01-17 14:02:00 +0300

    edit

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace Split
     {
         static void Main(string[] args)
         {
+            char splitElement = ' ';
             string text = "Дана строка с текстом, используя метод строки String.Split()" +
                 " получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
-            string[] subs = text.Split(' ');
+            string[] subs = text.Split(splitElement);
 
             foreach (string wrod in subs)
                 Console.WriteLine(wrod);
